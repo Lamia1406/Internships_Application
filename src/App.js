0d7711e4f05a4beaf219caae66b1cf1d477950b8
Lamia Hamdi <lamia.hamdi@univ-constantine2.dz>
2023-04-18 03:19:20 +0200

    Add routes to the application

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
 import React from 'react';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Home from './Pages/home';
 import Product from './Pages/product';
-import Home from './Pages/home'
-import {Router, Routes, Route } from 'react-router-dom';
-
+import Login from './Pages/login';
+import Internships from './Pages/internships';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import NavBar from './partials/navbar';
+import YourApplication from './Pages/yourApp';
+import ApplyForInternship from './Pages/applyForInternship';
+import Footer from './partials/footer';
+import Students from './Pages/students';
 
 function App() {
- 
-  return(
-    <Product/>
+  const location = useLocation(); // Get the current location
+
+  const isLoginPage = location.pathname === '/login';
+
+  return (
+    <>
+     
+      {!isLoginPage && <NavBar />}
+      <Routes>
+        <Route path='login' element={<Login />} />
+        <Route path='/' element={<Home />} />
+        <Route path='product' element={<Product />} />
+        <Route path='internships' element={<Internships />} />
+        <Route path='yourApp' element={<YourApplication />} />
+        <Route path='applyForInternship' element={<ApplyForInternship />} />
+        <Route path='students' element={<Students />} />
+      </Routes>
+      {!isLoginPage && <Footer />}
+    </>
   );
 }
 
