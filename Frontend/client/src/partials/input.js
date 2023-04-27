e6f29a6036cc2a2f0e8df96d514df2649b848df7
Lamia Hamdi <lamia.hamdi@univ-constantine2.dz>
2023-04-27 11:49:34 +0200

    Change the structure of the frontend

--- Frontend/client/src/partials/input.js
+++
@@ -1,7 +0,0 @@
-import inputClass from "../Styles/partials/input.module.css";
-function Input(props){
-    return(
-        <input className={`form-control me-4 ${inputClass.input}`} type={props.type} placeholder={props.placeholder} aria-label="Search"  value ={props.value} onChange={props.onChange}/>      
-    );
-}
-export default Input;
--- Frontend/client/src/partials/textarea.js
+++
@@ -1,7 +0,0 @@
-import textClass from "../Styles/partials/textarea.module.css"
-function TextArea(props){
-    return(
-        <textarea className={`form-control ${textClass.textarea}`}    type="text" maxLength={props.maxLength} placeholder={props.placeholder}/>
-    );
-}
-export default TextArea;
